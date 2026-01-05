833cab10ca61b9e58d0f6f953c4a01ef3c944dd6
ifamzain-stack <ifamzain@gmail.com>
2026-01-05 11:23:57 +0000

    inital commit

---
+++ string.py
@@ -0,0 +1,13 @@
+print("HELLO WORLD".lower())
+print("python".upper())
+print("I love python".split())
+print("-".join(["I", "love", "python"]))
+print("Python".find("o"))
+print("I like Java".replace("Java", "Python"))
+print(len("programming"))
+print("Python" in "I love python")
+text = str(input("Enter a string: "))
+revText = text[::-1]
+text = revText
+print("Reverse of given string is: ")
+print(text)
